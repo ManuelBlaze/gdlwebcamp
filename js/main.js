483934b3b7c0c783ca2d5dd571f4fbebbc4a908f
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-12 22:18:28 -0500

    Leaflet api map

--- js/main.js
+++ js/main.js
@@ -5,6 +5,18 @@
 
   document.addEventListener('DOMContentLoaded', function () {
 
+    //Mapa
+    var map = L.map('mapa').setView([6.243084, -75.576308], 17);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([6.243084, -75.576308]).addTo(map)
+      .bindPopup('GDLWebCamp 2020<br> Plaza Mayor <br>Boletos ya disponibles.')
+      .openPopup()
+
+
     //Campos Usuario
     let nombre = document.getElementById('nombre');
     let apellido = document.getElementById('apellido');
