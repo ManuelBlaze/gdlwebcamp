0029e1e20291f4594a44248175ef577fac260d66
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-15 17:21:26 -0500

    jQuery Plugins
    Final cowntdown, animate Number

--- js/main.js
+++ js/main.js
@@ -147,5 +147,27 @@ $(function () {
     $(enlace).fadeIn(1000);
 
     return false;
-  })
+  });
+
+  //Animaciones parallax numeros
+  $('.resumen-evento li:nth-child(1) p').animateNumber({
+    number: 6
+  }, 1200);
+  $('.resumen-evento li:nth-child(2) p').animateNumber({
+    number: 15
+  }, 1200);
+  $('.resumen-evento li:nth-child(3) p').animateNumber({
+    number: 3
+  }, 1500);
+  $('.resumen-evento li:nth-child(4) p').animateNumber({
+    number: 9
+  }, 1500);
+
+  //Cuenta regresiva
+  $('.cuenta-regresiva').countdown('2020/12/10 09:00:00', function (e) {
+    $('#dias').html(e.strftime('%D'));
+    $('#horas').html(e.strftime('%H'));
+    $('#minutos').html(e.strftime('%M'));
+    $('#segundos').html(e.strftime('%S'));
+  });
 });
