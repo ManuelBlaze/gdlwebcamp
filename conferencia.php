c712e3085b958dd87f26f7d7081a32b3274a28cb
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-16 23:44:38 -0500

    php

---
+++ conferencia.php
@@ -0,0 +1,50 @@
+<!--HEADER-->
+<?php include_once 'includes/templates/header.php'; ?>
+
+    <section class="contenedor seccion">
+        <h2>La mejor conferencia de diseño web en español</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi reiciendis ducimus quam deleniti maiores
+            aliquid in laboriosam dolores, dolorum iusto ratione alias voluptates harum sunt velit iure aliquam ut
+            consequatur.</p>
+    </section>
+    <!--seccion-->
+
+    <div class="seccion contenedor">
+        <h2>Galería de Fotos</h2>
+        <div class="galeria">
+            <a href="img/galeria/01.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/01.jpg">
+            </a>
+            <a href="img/galeria/02.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/02.jpg">
+            </a>
+            <a href="img/galeria/03.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/03.jpg">
+            </a>
+            <a href="img/galeria/04.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/04.jpg">
+            </a>
+            <a href="img/galeria/05.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/05.jpg">
+            </a>
+            <a href="img/galeria/06.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/06.jpg">
+            </a>
+            <a href="img/galeria/07.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/07.jpg">
+            </a>
+            <a href="img/galeria/08.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/08.jpg">
+            </a>
+            <a href="img/galeria/09.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/09.jpg">
+            </a>
+            <a href="img/galeria/10.jpg" data-lightbox="galeria">
+                <img src="img/galeria/thumbs/10.jpg">
+            </a>
+        </div>
+    </div>
+
+<!--FOOTER-->
+<?php include_once 'includes/templates/footer.php'; ?>   
+    
