1f5e28359d2c3fb6c794a999d8089ca3e53fa255
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-24 21:42:38 -0500

    Navbar

--- admin/templates/aside.php
+++ admin/templates/aside.php
@@ -1,9 +1,9 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="../../index3.html" class="brand-link">
+      <a href="admin-area.php" class="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
           style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">GloWebCamp</span>
       </a>
 
       <!-- Sidebar -->
@@ -11,10 +11,10 @@
         <!-- Sidebar user (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            <img src="dist/img/userblaze.jpg" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
-            <a href="#" class="d-block">Alexander Pierce</a>
+            <a href="#" class="d-block">Manuel Blaze</a>
           </div>
         </div>
 
